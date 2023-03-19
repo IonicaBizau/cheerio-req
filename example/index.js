e1f97d75a5d65d6e36a00e2663706719285b755a
Ionică Bizău <bizauionica@gmail.com>
2023-03-19 15:12:11 +0100

    Rewrite using axios

--- example/index.js
+++ example/index.js
@@ -2,7 +2,8 @@
 
 const cheerioReq = require("../lib");
 
-cheerioReq("http://ionicabizau.net", (err, $) => {
+(async () => {
+    const { $ } = await cheerioReq("http://ionicabizau.net")
     let $links = $("a.article-title");
     for (let i = 0; i < $links.length; ++i) {
         console.log($links.eq(i).text());
@@ -11,4 +12,4 @@ cheerioReq("http://ionicabizau.net", (err, $) => {
     // Pi Day, Raspberry Pi and Command Line
     // How I ported Memory Blocks to modern web
     // How to convert JSON to Markdown using json2md
-});
+})()
